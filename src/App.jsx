dabeaf4c517009d1f056852921ee104ced8f001b
crlsdlgdkruger <cdelgado@krugerschool.edu.ec>
2024-12-29 15:06:07 -0500

    add login

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './routes/AppRouter';
-
+import 'primereact/resources/primereact.min.css';
+import 'primereact/resources/themes/lara-light-teal/theme.css';
+import 'primeicons/primeicons.css';
 
 function App() {
   return (
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,24 @@
+import { Button } from 'primereact/button';
+import { FloatLabel } from 'primereact/floatlabel';
+import { InputText } from 'primereact/inputtext';
+import "./login.css"
+
 export const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className='login-container'>
+      <div className=''>
+        <h2>Sign In</h2>
+        <FloatLabel>
+          <InputText id="email" value={""} onChange={(e) => { }} />
+          <label htmlFor="email">Email</label>
+        </FloatLabel>
+        <FloatLabel>
+          <InputText id="password" value={""} onChange={(e) => { }} />
+          <label htmlFor="password">Password</label>
+        </FloatLabel>
+        <Button icon="pi pi-sign-in" label="Login" iconPos="right" />
+        <p>Don't have an account yet? <a href='/register' className='redirect-link'>Sign Up</a></p>
+      </div>
     </div>
   )
 }
